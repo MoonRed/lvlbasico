807271bddad52f8a74cc2d86384adb319ee66837
Moon Antonio <pinchodev91@gmail.com>
2018-01-23 19:43:44 +0100

    Estructuras condicionales

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/03.Estructuras Condicionales/Condicionales.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Condicionales : MonoBehaviour
+{
+	public string texto1 = string.Empty;
+	public string texto2 = string.Empty;
+
+
+	// Estructuras Condicionales
+	/*
+	 if -> Si
+	 if/else -> Si/Sino
+	 switch -> Condiciones
+	*/
+
+	void Start ()
+	{
+		if (texto1 == texto2)
+		{
+			Debug.Log("Que es igual!");
+		}
+		else {
+			Debug.Log("No es igual!");
+		}
+
+		switch (texto1)
+		{
+			case "Texto1":
+				Debug.Log("Texto1");
+				break;
+
+			case "Texto2":
+				Debug.Log("Texto2");
+				break;
+
+			default:
+				Debug.Log("Default");
+				break;
+		}
+	}
+}
