d973d99843f69f6dfe53323ce482c0290415b1b7
Moon Antonio <pinchodev91@gmail.com>
2018-01-23 11:36:17 +0100

    Tipos variables

--- LvlBasico Project/Assets/Proyecto LvlBasico/01.Pseudocodigo/Pseudocodigo.cs
+++ LvlBasico Project/Assets/Proyecto LvlBasico/01.Pseudocodigo/Pseudocodigo.cs
@@ -12,4 +12,27 @@ public class Pseudocodigo : MonoBehaviour
 	 04
 	 05
 	 */
+
+	// <Variables>
+	// ...
+
+	// <tipo> <nombre>
+	// int entero;
+
+	/* Tipos Variables
+	Tipos numericos:
+
+	int = entero; (0,1,2,3)
+	float = decimal; (0.1,0.2)
+	double = decimal; (0.1111111)
+
+	Tipos caracteres:
+
+	char = caracter; (A,B,C)
+	string = cadena de caracteres; (Cadena,De)
+
+	Tipos auxiliar:
+	bool = true, false;
+	 */
+
 }
