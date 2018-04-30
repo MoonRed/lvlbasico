6663c053ed7c1a49f618821647d0125927521ae1
Moon Antonio <pinchodev91@gmail.com>
2018-04-30 20:57:36 +0200

    Operadores

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/02.Operadores/Operadores.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Operadores : MonoBehaviour
+{
+	// <Operadores Aritmeticos>
+
+	/*
+		Suma		-> +
+		Resta		-> -
+		Multi		-> *
+		Division	-> /
+		Modulo		-> %
+	*/
+
+	// <Operadores Logicos>
+
+	/*
+		y					-> &&
+		o					-> ||
+		Negacion			-> !
+		Igualdad			-> ==
+		Si no es igual		-> !=
+		Menor				-> <
+		Mayor				-> >
+		Menor igual			-> <=
+		Mayor igual			-> >=
+	*/
+
+	public int x = 0;
+	public int y = 0;
+	public int total = 0;
+
+	private void Start()
+	{
+		total = x + y;
+		Debug.Log(total);
+
+		// Logico
+		if (total != x)
+		{
+			Debug.Log("No es igual!");
+		}
+	}
+}
