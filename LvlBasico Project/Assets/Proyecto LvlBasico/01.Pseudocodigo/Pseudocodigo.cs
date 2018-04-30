de9e719f99dfe04c8497dfe406422afb44690b79
Moon Antonio <pinchodev91@gmail.com>
2018-04-30 20:02:57 +0200

    Add Comentarios

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/01.Pseudocodigo/Pseudocodigo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Pseudocodigo : MonoBehaviour
+{
+	// <Comentarios>
+
+	// Comentario simple (Solo comenta una linea)
+	// Tengo que colocar otro comentario
+
+	/*
+	 Comentario multiple o bloque de comentario
+	 (Nos deja comentar varias lineas)
+	 */
+}
