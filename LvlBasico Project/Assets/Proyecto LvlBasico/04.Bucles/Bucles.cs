6295b4296f4e3445dcf67de5141d41b29c3a2e1a
Moon Antonio <pinchodev91@gmail.com>
2018-04-30 21:19:34 +0200

    Bucles

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/04.Bucles/Bucles.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Bucles : MonoBehaviour
+{
+	public int experiencia = 0;
+	public bool auxiliar = true;
+	public List<int> coleccion = new List<int>();
+
+	private void Start()
+	{
+		// for
+		for (int n = 0; n < 10; n++)
+		{
+			experiencia = experiencia + 1;
+		}
+
+		// while
+		while (auxiliar)
+		{
+			experiencia++;
+
+			if (experiencia >= 30)
+			{
+				auxiliar = false;
+			}
+		}
+
+		// foreach
+		foreach (int valor in coleccion)
+		{
+
+		}
+	}
+}
