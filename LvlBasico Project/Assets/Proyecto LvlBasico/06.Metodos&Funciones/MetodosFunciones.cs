19dd2ec9910c6c3b6407e2f0e4b65b3d3a063b22
Moon Antonio <pinchodev91@gmail.com>
2018-04-30 21:52:01 +0200

    Metodos y funciones

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/06.Metodos&Funciones/MetodosFunciones.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class MetodosFunciones : MonoBehaviour
+{
+	/// <summary>
+	/// <para>Variables para el metodo <see cref="Metodo"/>.</para>
+	/// </summary>
+	protected int val = 0;
+
+	public void Start()
+	{
+		val = 1;
+
+		// Un metodo es un bloque de codigo que contiene una serie de instrucciones
+		Metodo("Antonio",0);
+
+		// Una funcion es un bloque de codigo que nos devuelve un valor
+		Debug.Log(Funcion(5,4).ToString());
+	}
+
+	#region Metodos
+	/// <summary>
+	/// <para>Este metodo, realiza esta accion.</para>
+	/// <para> El metodo es de <see cref="MetodosFunciones"/>.</para>
+	/// </summary>
+	public void Metodo()
+	{
+
+	}
+
+	public void Metodo(string nombre)
+	{
+
+	}
+
+	public void Metodo(string nombre, int valor)
+	{
+		Debug.Log("Llamada desde Start()");
+	}
+	#endregion
+
+	#region Funciones
+	public int Funcion(int x,int y)
+	{
+		return x + y;
+	}
+	#endregion
+}
