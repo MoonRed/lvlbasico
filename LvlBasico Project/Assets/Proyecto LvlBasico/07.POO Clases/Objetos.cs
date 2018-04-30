fe604e3824b6e85f7d2c8906e08250569c876f53
Moon Antonio <pinchodev91@gmail.com>
2018-04-30 23:01:26 +0200

    POO

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/07.POO Clases/Objetos.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Autor:			Antonio
+ * Email:				...		
+ * Fecha:
+ * Descripcion:
+ */
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace Clases.lvlBasico
+{
+	/// <summary>
+	/// <para>Clase auxiliar para generar objetos.</para>
+	/// </summary>
+	public class Objetos : MonoBehaviour
+	{
+		private void Start()
+		{
+			Personaje personaje = new Personaje(10f, 10f, 5f, 5f);
+
+			personaje.VidaActual = 10f;
+			personaje.Atacar();
+		}
+	}
+}
---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/07.POO Clases/Personaje.cs
@@ -0,0 +1,54 @@
+﻿#region Librerias
+using UnityEngine;
+using Clases.lvlBasico;
+#endregion
+
+/// <summary>
+/// <para>Clase principal del personaje.</para>
+/// </summary>
+public class Personaje : MonoBehaviour
+{
+	#region Variables
+	/// <summary>
+	/// <para>Vida actual del personaje.</para>
+	/// </summary>
+	private float vidaActual = 10.0f;							// Vida actual del personaje
+	/// <summary>
+	/// <para>Vida maxima del personaje.</para>
+	/// </summary>
+	private float vidaMax = 10.0f;								// Vida maxima del personaje
+	/// <summary>
+	/// <para>Mana actual del personaje.</para>
+	/// </summary>
+	private float manaActual = 5.0f;							// Mana actual del personaje
+	/// <summary>
+	/// <para>Mana maximo del personaje.</para>
+	/// </summary>
+	private float manaMax = 5.0f;                               // Mana maximo del personaje
+	#endregion
+
+	#region Propiedades
+	public float VidaActual
+	{
+		get { return vidaActual; }
+		set { vidaActual = value; }
+	}
+	#endregion
+
+	#region Constructor
+	public Personaje(float vidActual, float vidMax, float manActual, float manMax)
+	{
+		VidaActual = vidActual;
+	}
+	#endregion
+
+	#region Metodos
+	private void Start()
+	{
+		Debug.Log("Vida Actual " + VidaActual);
+	}
+
+	public void Atacar()
+	{ }
+	#endregion
+}
