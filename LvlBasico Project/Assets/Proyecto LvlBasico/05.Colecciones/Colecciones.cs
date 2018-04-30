f29f3b9fb05adda7520ba24cf24491d15a67a41a
Moon Antonio <pinchodev91@gmail.com>
2018-04-30 21:37:35 +0200

    Colecciones

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/05.Colecciones/Colecciones.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Colecciones : MonoBehaviour
+{
+	#region Arrays
+	// <Array>
+	public int[] array = new int[100];
+	#endregion
+	#region Listas
+	// <Lista>
+	public List<int> lista = new List<int>();
+	#endregion
+
+	/* Metodos Listas
+		Add
+		Remove
+		Clear
+		Count
+		...
+	*/
+
+	private void Start()
+	{
+		array[0] = 12;
+		lista.Add(0);
+	}
+
+}
