ff73c1724025759a0c3b2af9dbdce04592ef102d
Moon Antonio <pinchodev91@gmail.com>
2018-04-30 20:15:35 +0200

    Pseudocodigo

--- LvlBasico Project/Assets/Proyecto LvlBasico/01.Pseudocodigo/Pseudocodigo.cs
+++ LvlBasico Project/Assets/Proyecto LvlBasico/01.Pseudocodigo/Pseudocodigo.cs
@@ -11,4 +11,32 @@ public class Pseudocodigo : MonoBehaviour
 	 Comentario multiple o bloque de comentario
 	 (Nos deja comentar varias lineas)
 	 */
+
+	// <Variables>
+
+	// <ACCESIBILIDAD> <TIPO> <NOMBRE>
+	public int entero;
+	public string texto;
+	public float numDecimal;
+
+	// <Inicializacion de variables>
+
+	public int enteroIni = 23;
+	public string textoIni = "Texto";
+	public float numDecimalIni = 23f;
+
+	// <Nomenclatura>
+
+	// PascalCasing
+	//Metodos y funciones, la primera letra de cada palabra en mayusculas.
+	public void GenerarProceso() { }
+
+	// CamelCasing
+	// Variables, la primera en minuscula y las siguientes en masyuscula.
+	public int hpBar;
+	public string nombreUsuario;
+
+	// <Marcador>
+
+	// TODO seguir con el curso
 }
