a0d6b36bc508991d9a7a4e91efc678dcd9b88ad3
Moon Antonio <pinchodev91@gmail.com>
2018-05-01 02:37:59 +0200

    Abstraccion

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/09.Abstraccion/Aventurero.cs
@@ -0,0 +1,33 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Aventurero.cs (01/05/2018)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Clase hija de unidad.										\\
+// Fecha Mod:		01/05/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace Clases.lvlBasico
+{
+	/// <summary>
+	/// <para>Clase hija de unidad.</para>
+	/// </summary>
+	public class Aventurero : UnidadAbstracta 
+	{
+		public override void Nacer()
+		{
+			
+		}
+
+		public override void Andar()
+		{
+			base.Andar();
+
+			Debug.Log("Andando.");
+		}
+	}
+}
---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/09.Abstraccion/UnidadAbstracta.cs
@@ -0,0 +1,28 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// UnidadAbstracta.cs (01/05/2018)												\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Superclase unidad, contiene las propiedades comunes.		\\
+// Fecha Mod:		01/05/2018													\\
+// Ultima Mod:		Version Inicial.											\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace Clases.lvlBasico
+{
+	/// <summary>
+	/// <para>Superclase unidad, contiene las propiedades comunes.</para>
+	/// </summary>
+	public abstract class UnidadAbstracta
+	{
+		public abstract void Nacer();
+
+		public virtual void Andar()
+		{
+			Debug.Log("Andando.");
+		}
+	}
+}
