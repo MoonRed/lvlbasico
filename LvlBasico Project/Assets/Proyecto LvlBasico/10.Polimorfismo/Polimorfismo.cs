4ceaf60aefa3468b04691c8fb2cf3339ca90076d
Moon Antonio <pinchodev91@gmail.com>
2018-05-01 02:41:22 +0200

    Polimorfismo

---
+++ LvlBasico Project/Assets/Proyecto LvlBasico/10.Polimorfismo/Polimorfismo.cs
@@ -0,0 +1,34 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Polimorfismo.cs (01/05/2018)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Explicacion de polimorfismo.								\\
+// Fecha Mod:		01/05/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections.Generic;
+#endregion
+
+namespace Clases.lvlBasico
+{
+	/// <summary>
+	/// <para>Explicacion de polimorfismo.</para>
+	/// </summary>
+	public class Polimorfismo : MonoBehaviour 
+	{
+		public void Buscar()
+		{ }
+
+		public void Buscar(int x)
+		{ }
+
+		public void Buscar(string[] array)
+		{ }
+
+		public void Buscar(List<string> lista)
+		{ }
+	}
+}
